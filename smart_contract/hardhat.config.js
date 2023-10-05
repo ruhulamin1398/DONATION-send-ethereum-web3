89a99851caf510883db63be1081745ec0967c43e
ruhulamin1398 <ruhul.ok@gmail.com>
2023-10-05 20:15:39 +0600

    controct deployed

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: '0.8.0',
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/TCj2T_EVQQiflTwIp3iYlr4O-jEENbNg',
+      accounts: ['20cec08faea52eb9f022ab7c0f79224d6fe8c1594e3126d6367a3c669080f33a'],
+    },
+  },
 };
