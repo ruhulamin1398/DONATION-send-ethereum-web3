2c0a83331de24d32f838478dee694af1a20f50db
ruhulamin1398 <ruhul.ok@gmail.com>
2023-10-06 17:30:19 +0600

    change deploy port number

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -117,7 +117,7 @@ TO SURVIVE AND THRIVE
                   onClick={handleSubmit}
                   className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
                 >
-                  Send now
+                  Donate Now
                 </button>
               )}
           </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  server: {
+    host: '0.0.0.0',  
+    port: 5555,      
+  },
   plugins: [react()]
 })
